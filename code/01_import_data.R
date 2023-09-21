10f84a0d3c6cec2bc1d6d48f84902403ce1048ae
professornaite <professornaite@gmail.com>
2023-09-21 00:46:24 -0400

    updates

--- code/01_import_data.R
+++ code/01_import_data.R
@@ -1,3 +1,5 @@
+## step01 import data
+
 # import the data
 fatal_raw <- read.csv("https://raw.githubusercontent.com/washingtonpost/data-police-shootings/master/v2/fatal-police-shootings-data.csv")
 
---
+++ code/02_process_data.R
@@ -0,0 +1,15 @@
+# step02 process data
+
+# install required packages
+install.packages("tidyverse")
+library(tidyverse)
+
+# load and check data
+df <- read.csv("data/fatal_raw.csv")
+df <- as_tibble(df)
+df
+
+# remove first column
+df <- df %>% 
+  select(-c(1))
+df
