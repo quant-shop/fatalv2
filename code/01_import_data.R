9801562d509fcdb2e193cc4d73b0a23600a51faa
professornaite <professornaite@gmail.com>
2023-09-21 00:39:03 -0400

    removing personal path directory

--- code/01_import_data.R
+++ code/01_import_data.R
@@ -2,4 +2,4 @@
 fatal_raw <- read.csv("https://raw.githubusercontent.com/washingtonpost/data-police-shootings/master/v2/fatal-police-shootings-data.csv")
 
 # write the csv file
-write.csv(fatal_raw, "/Users/nathanalexander/Dropbox/Projects/fatalv2/data/fatal_raw.csv")
+write.csv(fatal_raw, "data/fatal_raw.csv")
