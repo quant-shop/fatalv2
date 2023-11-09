cc0e59d0c9459f7d69d8ba9e056ba675be1c166b
professornaite <professornaite@gmail.com>
2023-11-09 02:20:05 -0500

    generating template code

--- code/05_census_data.R
+++ code/05_census_data.R
@@ -4,21 +4,33 @@
 ### The api.R script can be found here: https://github.com/quant-shop/fatalv2/blob/main/api.R
 ### by running the api.R script, you will load your census API key
 
-# create a sample list of a single variable
-varlist = data.frame(head(load_variables(2020, "acs5"), n=16))
+# install packages / un-comment where needed
+# install.packages("tidycensus")
+# install.packages("tidyverse")
+# install.packages("tidyr")
+
+# load libraries
+library(tidycensus)
+library(tidyverse)
+library(tidyselect)
+library(dplyr)
+library(tidyr)
+library(stringr)
+
+# load all variable labels for a specific year
+labels <- function(yyyy){
+  load_variables(yyyy, "acs5")
+}
+labels(2020) -> labels
+labels
+
+# select all variables with the same root string
+varlist = data.frame(variable=vars_select(labels$name, starts_with('B01001A')))
 varlist
 
-# loading variable
-var1 <- load_variables(2020, "acs5") %>% 
-  filter(name=="B01001A_001")
-var1
-
-# sex by age (White alone) in the year 2020
-var1.values <- get_acs(geography = "state",
-                             variables = "B01001A_001",
-                             year = 2020)
-var1.values %>% 
-  left_join(var1,
-            by = c("variable" = "name")) %>% 
-  mutate(year = 2020)
+# extract information from labels
+varlist1 <- labels %>% 
+  separate(label, c("l-pos1", "l-pos2", "l-pos3", "l-pos4", "l-pos5", "l-pos6", "l-pos7")) %>% 
+  select(-"l-pos1")
+varlist1
 
