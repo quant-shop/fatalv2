87a949a10895efd885606a8be104142a6208b3cc
professornaite <professornaite@gmail.com>
2023-11-08 21:47:23 -0500

    process data update

--- code/01_import_data.R
+++ code/01_import_data.R
@@ -1,18 +1,15 @@
 # step01 import data
 
-## import the v2 washington post fatal force data
+## ## ## import the v2 Washington Post fatal force data
 
 ### context: https://www.washingtonpost.com/graphics/investigations/police-shootings-database/
 ### content: https://github.com/washingtonpost/data-police-shootings
 ### v2 files: https://github.com/washingtonpost/data-police-shootings/tree/master/v2
-
-fatal_raw <- read.csv("https://raw.githubusercontent.com/washingtonpost/data-police-shootings/master/v2/fatal-police-shootings-data.csv")
+wapo_fatal_raw <- read.csv("https://raw.githubusercontent.com/washingtonpost/data-police-shootings/master/v2/fatal-police-shootings-data.csv")
 
 ## check the imported data
 head(fatal_raw)
 tail(fatal_raw)
 
 ## write a csv file
-write.csv(fatal_raw, "data/fatal_raw.csv")
-
-## import the 
+write.csv(fatal_raw, "data/fatal_raw_01.csv")
--- code/02_process_data.R
+++ code/02_process_data.R
@@ -8,8 +8,42 @@ library(tidyverse)
 df <- read.csv("data/fatal_raw.csv")
 df <- as_tibble(df)
 df
+tail(df) # view the tail of the data
+glimpse(df) # get a glimpse of the data
+str(df) # view the data structure
 
-# remove first column
-df <- df %>% 
-  select(-c(1))
+# fix vars
+# change vars to more appropriate formats
+df$date <- as.Date(df$date) # check/change to date format
+df$age <- as.numeric(df$age)
+df$was_mental_illness_related <- as.logical(df$was_mental_illness_related)
+df$body_camera <- as.logical(df$body_camera)
+str(df)
+
+# view a summary of the data
+summary(df)
+
+# create a two-column transfer df to match state to abb
+transfer <- tibble(state = state.name) %>% 
+  tibble(abb = state.abb) %>% 
+  bind_rows(tibble(state = "District Of Columbia", abb = "DC")) # add DC
+transfer
+tail(transfer)
+
+# add a state name variable to the fatal df
+df$state.name <- state.name[match(df$state, transfer$abb)]
+df %>% 
+  mutate(state.abb = state) %>% 
+  relocate(id, date, state.name, state.abb) -> df
 df
+
+# create a year column
+# format to 20YY
+df.year <- format(df$date, format="20%y") 
+df$year <- df.year # add column to df
+df %>% relocate(id, date, year, state.name, state.abb) -> df
+df
+tail(df)
+
+## write a csv file
+write.csv(df, "data/fatal_raw_02.csv")
