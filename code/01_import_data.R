ff416a86245288ec1fada857c6faa7127417571f
professornaite <professornaite@gmail.com>
2023-11-08 20:40:25 -0500

    step01 file updates

--- code/01_import_data.R
+++ code/01_import_data.R
@@ -1,7 +1,18 @@
-## step01 import data
+# step01 import data
 
-# import the data
+## import the v2 washington post fatal force data
+
+### context: https://www.washingtonpost.com/graphics/investigations/police-shootings-database/
+### content: https://github.com/washingtonpost/data-police-shootings
+### v2 files: https://github.com/washingtonpost/data-police-shootings/tree/master/v2
+
 fatal_raw <- read.csv("https://raw.githubusercontent.com/washingtonpost/data-police-shootings/master/v2/fatal-police-shootings-data.csv")
 
-# write the csv file
+## check the imported data
+head(fatal_raw)
+tail(fatal_raw)
+
+## write a csv file
 write.csv(fatal_raw, "data/fatal_raw.csv")
+
+## import the 
